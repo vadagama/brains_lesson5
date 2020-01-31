ba206768f17b45543cf99a8114785b018bc0a3b9
Oleg Krasnov <oleg@vadagama.ru>
2020-01-31 14:00:38 +0300

    Lesson 5 done

---
+++ animals/src/Horse.java
@@ -0,0 +1,9 @@
+public class Horse extends Animal {
+
+    public Horse(String name) {
+        super(name);
+        this.maxRunDistance = 1500;
+        this.maxSwimDistance = 100;
+        this.maxJumpHeight = 3;
+    }
+}
